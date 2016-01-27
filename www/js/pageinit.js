69fba406b4d75211544c71862c1cf15ee53cab48
SergioRep <SergioAvendano@MacBook-Pro-de-Sergio.local>
2016-01-27 16:48:26 -0300

    se agregaron las notificaciones

--- www/js/pageinit.js
+++ www/js/pageinit.js
@@ -417,5 +417,15 @@
             var ctx = document.getElementById("seguimiento-jugador-graf").getContext("2d");
             var newChart = new Chart(ctx).Line(lineChartData);
         }
+        if(activePage === 'notificaciones'){
+            var notifica = new notificaciones();
+            notifica.getNotificaciones();
+            delete notifica;
+        }
+        if(activePage === 'asistencia'){
+            var asistencia = new notificaciones();
+            asistencia.getAsistencia();
+            delete asistencia;
+        }
 
     });
